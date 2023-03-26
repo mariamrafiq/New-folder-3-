723e55440b573a8b91719ad38b03d70647be4857
Tawakkal laptops <mariamrafique@gmail.com>
2023-03-26 11:39:13 +0500

    done

---
+++ app.js
@@ -0,0 +1,95 @@
+// alert("Hello world");
+//console.log("Hello world");
+// console.log(a); //a
+// alert(a);
+// var a = 1 // integer
+// console.log(a); //a
+
+
+// var a = "Ali 121234" //string
+// console.log(a);
+
+// var num1 = 5;
+// var num2 = 10;
+// var sum= num1+num2
+// console.log("sum"+ sum); // concatination string to string string to integer
+// //  sum = 15
+
+// var name="ALI";
+// var num1 = 7;
+// var sum =name1+7
+// console.log(sum);
+// var num1=10;
+// var num2= 15;
+// console.log(num1-num2)
+// document.write(num1-num2)
+// var num1 = 15
+// var num2 = 2
+// var reminder =num1 % num2;
+// console.log( num1 + " % " + num2 + " = "+ reminder + " 1 ") 
+
+
+var num = 10;
+var newnum = num++;// first update the value 
+console.log(newnum);// then print it
+// alert(num)
+console.log(num);
+
+
+var num = 10;
+var newnum = num--;// first update the value 
+console.log(newnum);// then print it
+// alert(num)
+console.log(num);
+
+
+var num = 10;
+var newnum = --num;// first update the value 
+console.log(newnum);// then print it
+// alert(num)
+// console.log(num);
+
+
+
+
+// var expression = (1-3)*4;
+// console.log(expression)
+
+
+
+
+//  var message = "Thanks, ";
+//  var userName = "Susan";
+//  var banger = "!";
+// alert(message  + userName + banger +"......" )
+
+
+
+// var firstName =prompt ("Enter Your Good Name")
+
+// alert(firstName);
+
+
+// var question = "Your species?";
+//  var defaultAnswer = "human";
+//  var spec = prompt(question, defaultAnswer);
+
+
+// var num1 = +prompt("Enter first number");
+// var num2 = +prompt("Enter second number","your number?");
+// var sum = num1 + num2;
+// alert(sum);
+
+if (5 == 5) {
+    alert("correct")
+}
+var perc = +prompt("enter your percentage","your percentage")
+
+
+
+var array=[]
+console.log(array)
+ var array=[0]='amna'
+ var array=[1]="fareeha"
+ var array=[2]='sahr'
+console.log(array)
